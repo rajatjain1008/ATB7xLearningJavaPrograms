3f17275d0015350599673eaee8aa81a381f04388
Rajat Jain <rajatjain4294@gmail.com>
2024-07-11 00:46:28 +0530

    All files are added for 7th July

---
+++ src/ex_07072024/Lab005.java
@@ -0,0 +1,14 @@
+package ex_07072024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        // commenting
+
+        /**
+         * Multi comments
+         *
+         *
+         *
+         */
+    }
+}
